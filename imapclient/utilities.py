a8205f8a3daa91edce2ced8e7381a812ceb3eff7
Santiago Gilabert <santiagogilabert@gmail.com>
2017-08-24 12:03:31 -0300

    Fixed an issue where it was failing to read the emails in some cases

--- imapclient/utilities.py
+++ imapclient/utilities.py
@@ -57,22 +57,25 @@ def read_mailbox(account, password):
 
     for i in range(latest_email_id, latest_email_id - QUANTITY, -1):
 
-        typ, data = mail.fetch(i, MESSAGE_PARTS)
+        try :
+            typ, data = mail.fetch(i, MESSAGE_PARTS)
 
-        subject = ''
-        sender = ''
+            subject = ''
+            sender = ''
 
-        for response_part in data:
+            for response_part in data:
 
-            if isinstance(response_part, tuple):
-                msg = email.message_from_string(response_part[1])
-                subject = msg['subject']
-                sender = msg['from']
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(response_part[1])
+                    subject = msg['subject']
+                    sender = msg['from']
 
-        # add ellipsis (...) if subject length is greater than 35 characters
-        if len(subject) > 35:
-            subject = '%s...' % subject[0:32]
+            # add ellipsis (...) if subject length is greater than 35 characters
+            if len(subject) > 35:
+                subject = '%s...' % subject[0:32]
 
-        result.append(dict(sender=sender, subject=subject))
+            result.append(dict(sender=sender, subject=subject))
+        except Exception, e:
+            pass
 
     return result
